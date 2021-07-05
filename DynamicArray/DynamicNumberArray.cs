3be036d18af44d34942b50e558c8774636a24f65
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-07-05 15:10:48 +0530

    DynamicArray Program

---
+++ DynamicArray/DynamicNumberArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicArray
+{
+    public class DynamicNumberArray
+    {
+
+        int[] buffer;
+        public DynamicNumberArray()
+        {
+            buffer = new int[5];
+
+        }
+        public DynamicNumberArray(int initialSize) { buffer = new int[initialSize]; }
+        public int this[int index]
+        {
+            set
+            {
+                if (index >= buffer.Length)
+                {
+                    Array.Resize(ref buffer, index + 2);
+                }
+
+                buffer[index] = value;
+            }
+            get
+            {
+                return buffer[index];
+            }
+        }
+    }
+}
---
+++ DynamicArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DynamicArray
+{
+    public class Program
+    {
+        static void Main()
+        {
+            DynamicNumberArray _dynamic = new DynamicNumberArray();
+            _dynamic[0] = 10;
+            _dynamic[1] = 20;
+            _dynamic[2] = 30;
+            _dynamic[3] = 40;
+            _dynamic[4] = 50;
+            _dynamic[5] = 60;
+
+            int value = _dynamic[5];
+            Console.WriteLine(value);
+        }
+    }
+}
+    
+
